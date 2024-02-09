d3e1355d494608d8692a0f0a15b61c2f7185b32b
Armin R <66879529+datasmith-neef@users.noreply.github.com>
2024-02-09 15:12:56 +0100

    Update streamlitAndCustomGpt.py
    admin

--- streamlitAndCustomGpt.py
+++ streamlitAndCustomGpt.py
@@ -11,7 +11,15 @@ import os
 load_dotenv()  # Lädt die Variablen aus der .env-Datei
 
 customgpt_api_key = os.getenv('CUSTOMGPT_API_KEY')
+activation_code_1 = os.getenv('ACTIVATION_CODE_FAC')
+activation_code_2 = os.getenv('ACTIVATION_CODE_SIN')
+activation_code_3 = os.getenv('ACTIVATION_CODE_DSO')
 
+# App title
+st.set_page_config(page_title="Datasmith - GPT Chatbot",page_icon='💬')
+
+st.markdown('<div class="typewriter-text">👾 Factor.Design-GPT</div>', unsafe_allow_html=True)
+st.caption("🚀 A chatbot powered by OpenAI LLM and Datasmith Office")
 
 # Function to retrieve citations using CustomGPT API
 def get_citations(api_token, project_id, citation_id):
@@ -64,8 +72,7 @@ def clear_chat_history():
     st.session_state.session_id = str(uuid.uuid4())
     st.session_state.messages = [{"role": "assistant", "content": "Moin , ich bin Factor-GPT , na?"}]
 
-# App title
-st.set_page_config(page_title="Factor - GPT Chatbot")
+
 
 # Check if session_id is not in session_state, initialize it
 if "session_id" not in st.session_state:
@@ -73,22 +80,52 @@ if "session_id" not in st.session_state:
 
 # CustomGPT Credentials
 with st.sidebar:
-    st.title('Factor-GPT (Settings)')
+    ts = 'Datasmith-GPT (Settings)'
+    sidebar_header = st.header(ts)
     st.image("compressed_office_image.jpg")
    
     st.markdown("Hier können X weitere Projekte hinzugefügt werden - Demoversion - Datasmith Office")
     #customgpt_api_key = st.text_input('Enter CustomGPT API Key:', type='password')
     customgpt_api_key = customgpt_api_key
-    if customgpt_api_key:
+    activation_code = st.text_input('Enter youre activation_code : ',type = 'password')
+
+    selected_index = ''
+    if activation_code == activation_code_1:
+        selected_index = 1
+        st.success("Activated")
+        sidebar_header.header("Factor.design-GPT (Activated)")
+        
+    elif activation_code == activation_code_2:
+        selected_index = 0
+        st.success("Activated")
+        sidebar_header.header("Sintronics-GPT (Activated)")
+
+    elif activation_code == activation_code_3:
+        selected_index = 77
+        st.success("Activated")
+        sidebar_header.header("Datasmith-GPT (Admin-Mode)")
+
+    else : st.error('No projects found. Please check your actication key.', icon='❌')
+    
+    
+
+
+    if customgpt_api_key and selected_index:
         st.subheader('Select Project')
         listProject = get_projectList(customgpt_api_key)
         
         # Check if projects are available
         if listProject is not None:
             projectNames = [projt.project_name for projt in listProject]
-            selected_model = st.sidebar.selectbox('Select Model', projectNames, key='selected_model')
+            
+            selected_index = selected_index # Auswahl je nach Activation Codes
+            if selected_index != 77:
+                selected_model = st.sidebar.selectbox('Select Model', options=[projectNames[selected_index]], index=0, key='selected_model')
+            
+            else: selected_model = st.sidebar.selectbox('Select Model', projectNames, key='selected_model') # admin für alle projekte
             index = projectNames.index(selected_model)
             selected_project = listProject[index]
+            
         else:
             st.error('No projects found. Please check your API key.', icon='❌')
         
